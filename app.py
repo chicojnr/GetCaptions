61973e1928ba9d269f82623ae262e36f3d3e8187
chicojnr <souza.jnr@gmail.com>
2023-05-29 20:41:09 -0400

    include-subtitles

--- app.py
+++ app.py
@@ -1,10 +1,21 @@
 from flask import Flask
+from youtube_transcript_api import YouTubeTranscriptApi
+import json
 
 app = Flask(__name__)
 
 @app.get('/')
 def index():
-        return 'app ok'
+        srt = YouTubeTranscriptApi.get_transcript('7le4AGwtH94', languages=['pt'])
+        #with open("subtitles.txt", "w") as f:
+        caption_ret = []
+        # iterating through each element of list srt
+        for i in srt:
+                # writing each element of srt on a new line
+                #f.write("{}\n".format(i))
+                caption_ret.append(i)
+
+        return json.dumps(caption_ret, ensure_ascii=False)
 
 if __name__ == '__main__':
         app.run()
